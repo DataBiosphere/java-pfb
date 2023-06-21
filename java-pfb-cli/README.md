10087123be2402e4f354759c33ea13d1f1d39975
Shelby Holden <sholden@broadinstitute.org>
2023-06-21 09:33:26 -0400

    Hello world CLI
    add help command

---
+++ java-pfb-cli/README.md
@@ -0,0 +1,6 @@
+Current usage of CLI:
+First, run "fatJar" gradle task in java-pfb-cli
+
+Then, you can use the CLI with the following command:
+java -cp "java-pfb-cli/build/libs/java-pfb-cli.jar" JavaPfbCommand <command> <args>
+Only command right now: "hello"
---
+++ java-pfb-cli/src/main/java/JavaPfbCommand.java
@@ -0,0 +1,24 @@
+import picocli.CommandLine;
+
+import static picocli.CommandLine.Command;
+import static picocli.CommandLine.Option;
+
+@Command(name = "pfb")
+public class JavaPfbCommand implements Runnable {
+    public static void main(String[] args) {
+        CommandLine.run(new JavaPfbCommand(), args);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("A java implementation of pyPFB");
+    }
+
+    @Command(name = "hello")
+    public void helloCommand() {
+        System.out.println("Hello world!");
+    }
+
+    @Command(name = "--help")
+    public void helpCommand() { System.out.println("Help is on its way!! In the next PR...");}
+}
