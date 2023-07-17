22927aca9b84a2228b0932529c397f704ff191de
Shelby Holden <sholden@broadinstitute.org>
2023-07-17 11:16:51 -0400

    Dependency cleanup
    rework
    
    remove shellcheck

--- cli/src/main/java/JavaPfbCommand.java
+++ cli/src/main/java/JavaPfbCommand.java
@@ -1,24 +1,25 @@
-import picocli.CommandLine;
-
 import static picocli.CommandLine.Command;
-import static picocli.CommandLine.Option;
+
+import picocli.CommandLine;
 
 @Command(name = "pfb")
 public class JavaPfbCommand implements Runnable {
-    public static void main(String[] args) {
-        CommandLine.run(new JavaPfbCommand(), args);
-    }
+  public static void main(String[] args) {
+    CommandLine.run(new JavaPfbCommand(), args);
+  }
 
-    @Override
-    public void run() {
-        System.out.println("A java implementation of pyPFB");
-    }
+  @Override
+  public void run() {
+    System.out.println("A java implementation of pyPFB");
+  }
 
-    @Command(name = "hello")
-    public void helloCommand() {
-        System.out.println("Hello world!");
-    }
+  @Command(name = "hello")
+  public void helloCommand() {
+    System.out.println("Hello world!");
+  }
 
-    @Command(name = "--help")
-    public void helpCommand() { System.out.println("Help is on its way!! In the next PR...");}
+  @Command(name = "--help")
+  public void helpCommand() {
+    System.out.println("Help is on its way!! In the next PR...");
+  }
 }
