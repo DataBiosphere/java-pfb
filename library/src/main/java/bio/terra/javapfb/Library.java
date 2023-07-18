7371bb2602379f2d186da6a1413af9603ab661c9
Shelby Holden <sholden@broadinstitute.org>
2023-07-18 14:03:29 -0400

    Update main method so it's not empty

--- library/src/main/java/bio/terra/javapfb/Library.java
+++ library/src/main/java/bio/terra/javapfb/Library.java
@@ -1,5 +1,9 @@
 package bio.terra.javapfb;
 
 public class Library {
-  public static void main(String[] args) {}
+
+  // This is a stub app. It has no `main` and should never be run.
+  public static void main(String[] args) {
+    System.exit(-1);
+  }
 }
