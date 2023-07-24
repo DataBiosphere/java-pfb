b71b8794316bb8d344eec91a6b928e53227b1621
Shelby Holden <sholden@broadinstitute.org>
2023-07-24 14:36:26 -0400

    Remaining TODO

--- cli/src/main/java/bio/terra/pfb/PfbVersion.java
+++ cli/src/main/java/bio/terra/pfb/PfbVersion.java
@@ -3,8 +3,10 @@ package bio.terra.pfb;
 import picocli.CommandLine;
 
 public class PfbVersion implements CommandLine.IVersionProvider {
+
   @Override
   public String[] getVersion() {
-    return new String[] {"${COMMAND-FULL-NAME} ${JAVAPFB_VERSION_BUILD}"};
+    // TODO: Figure out how to get the version from the settings.gradle
+    return new String[] {"${COMMAND-FULL-NAME} 0.1.0"};
   }
 }
