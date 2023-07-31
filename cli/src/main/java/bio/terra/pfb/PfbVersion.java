7a1ad3e4f98a0171dd2cb3c357673634056c8630
bumptagbot <robot@terra.team>
2023-07-31 19:30:01 +0000

    bump 0.4.0

--- cli/src/main/java/bio/terra/pfb/PfbVersion.java
+++ cli/src/main/java/bio/terra/pfb/PfbVersion.java
@@ -6,7 +6,7 @@ public class PfbVersion implements CommandLine.IVersionProvider {
 
   // CLI_VERSION must be manually upgraded and resulting jar deployed to artifactory
   // See cli/README for publishing instructions
-  private static final String CLI_VERSION = "0.1.0";
+  private static final String CLI_VERSION = "0.4.0";
 
   @Override
   public String[] getVersion() {
