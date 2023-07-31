ae46970b9171d435d862303a0b821e9d2a50fed2
Shelby Holden <sholden@broadinstitute.org>
2023-07-31 17:00:18 -0400

    Pull version from git config

---
+++ cli/src/main/java/bio/terra/pfb/GitConfiguration.java
@@ -0,0 +1,40 @@
+package bio.terra.pfb;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class GitConfiguration {
+  private static final String GIT_PROPERTIES_FILE_NAME = "git.properties";
+  private static final String CLI_VERSION_BUILD_PROPERTY = "javapfb.cli.version.build";
+
+  public String getCliVersion() {
+    return readGitPropertiesValue(CLI_VERSION_BUILD_PROPERTY);
+  }
+
+  private String readGitPropertiesValue(String propertyName) {
+    String result = "";
+    InputStream inputStream = null;
+    try {
+      Properties prop = new Properties();
+      inputStream = getClass().getClassLoader().getResourceAsStream(GIT_PROPERTIES_FILE_NAME);
+      if (inputStream != null) {
+        prop.load(inputStream);
+      } else {
+        throw new FileNotFoundException(
+            "Property file '" + GIT_PROPERTIES_FILE_NAME + "' not found in the classpath");
+      }
+      return prop.getProperty(propertyName);
+    } catch (IOException e) {
+      System.out.println("Encountered exception while reading git properties: " + e);
+    } finally {
+      try {
+        inputStream.close();
+      } catch (IOException e) {
+        System.out.println(e);
+      }
+    }
+    return result;
+  }
+}
--- cli/src/main/java/bio/terra/pfb/PfbVersion.java
+++ cli/src/main/java/bio/terra/pfb/PfbVersion.java
@@ -4,12 +4,11 @@ import picocli.CommandLine;
 
 public class PfbVersion implements CommandLine.IVersionProvider {
 
-  // CLI_VERSION must be manually upgraded and resulting jar deployed to artifactory
-  // See cli/README for publishing instructions
-  private static final String CLI_VERSION = "0.1.0";
-
   @Override
   public String[] getVersion() {
-    return new String[] {"${COMMAND-FULL-NAME} " + CLI_VERSION};
+    GitConfiguration gitConfiguration = new GitConfiguration();
+    return new String[] {
+      String.format("${COMMAND-FULL-NAME} %s", gitConfiguration.getCliVersion())
+    };
   }
 }
