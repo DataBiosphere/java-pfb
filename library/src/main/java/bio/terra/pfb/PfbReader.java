abd7f8b1be1bc42fde1e7b29f0dcb46d77f9942d
Shelby Holden <sholden@broadinstitute.org>
2023-08-04 15:25:06 -0400

    Comment out unused code

--- library/src/main/java/bio/terra/pfb/PfbReader.java
+++ library/src/main/java/bio/terra/pfb/PfbReader.java
@@ -1,9 +1,6 @@
 package bio.terra.pfb;
 
-import bio.terra.pfb.exceptions.InvalidFileLocation;
 import java.io.*;
-import java.net.URL;
-import java.net.URLConnection;
 import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.generic.GenericDatumReader;
@@ -12,6 +9,7 @@ import org.apache.avro.io.DatumReader;
 
 public class PfbReader {
 
+  // TODO -- Need to figure out how to reference "Entity" from generated models
   //  public Entity deSerializeAvroHttpRequestJSON(byte[] data) throws IOException {
   //    DatumReader<Entity> reader = new SpecificDatumReader<>(Entity.class);
   //    Decoder decoder = null;
@@ -30,51 +28,52 @@ public class PfbReader {
     return dataFileReader.getSchema();
   }
 
-  public String readPFBFile(String fileLocation) throws IOException {
-    InputStream inputStream;
-    try {
-      if (isValidUrl(fileLocation)) {
-        inputStream = readFromSignedUrl(fileLocation);
-      } else {
-        inputStream = readFileFromClassPath(fileLocation);
-      }
-    } catch (Exception e) {
-      throw new InvalidFileLocation(fileLocation);
-    }
-    return readFromInputStream(inputStream);
-  }
-
-  boolean isValidUrl(String fileLocation) {
-    try {
-      new URL(fileLocation);
-      return true;
-    } catch (IOException e) {
-      return false;
-    }
-  }
-
-  InputStream readFromSignedUrl(String signedUrl) throws IOException {
-    URL urlObject = new URL(signedUrl);
-    URLConnection urlConnection = urlObject.openConnection();
-    return urlConnection.getInputStream();
-  }
-
-  InputStream readFileFromClassPath(String filePath) {
-    Class clazz = PfbReader.class;
-    return clazz.getResourceAsStream(filePath);
-  }
-
-  String readFromInputStream(InputStream inputStream) throws IOException {
-    StringBuilder resultStringBuilder = new StringBuilder();
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-      String line;
-      // TODO - we probably want to do something per line
-      while ((line = br.readLine()) != null) {
-        resultStringBuilder.append(line).append("\n");
-      }
-    } catch (IOException e) {
-      throw new IOException("Error reading from input stream", e);
-    }
-    return resultStringBuilder.toString();
-  }
+  // TODO - Can probably remove the code below as the there is data file reader from Apache Avro
+  //  public String readPFBFile(String fileLocation) throws IOException {
+  //    InputStream inputStream;
+  //    try {
+  //      if (isValidUrl(fileLocation)) {
+  //        inputStream = readFromSignedUrl(fileLocation);
+  //      } else {
+  //        inputStream = readFileFromClassPath(fileLocation);
+  //      }
+  //    } catch (Exception e) {
+  //      throw new InvalidFileLocation(fileLocation);
+  //    }
+  //    return readFromInputStream(inputStream);
+  //  }
+  //
+  //  boolean isValidUrl(String fileLocation) {
+  //    try {
+  //      new URL(fileLocation);
+  //      return true;
+  //    } catch (IOException e) {
+  //      return false;
+  //    }
+  //  }
+  //
+  //  InputStream readFromSignedUrl(String signedUrl) throws IOException {
+  //    URL urlObject = new URL(signedUrl);
+  //    URLConnection urlConnection = urlObject.openConnection();
+  //    return urlConnection.getInputStream();
+  //  }
+  //
+  //  InputStream readFileFromClassPath(String filePath) {
+  //    Class clazz = PfbReader.class;
+  //    return clazz.getResourceAsStream(filePath);
+  //  }
+  //
+  //  String readFromInputStream(InputStream inputStream) throws IOException {
+  //    StringBuilder resultStringBuilder = new StringBuilder();
+  //    try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+  //      String line;
+  //      // TODO - we probably want to do something per line
+  //      while ((line = br.readLine()) != null) {
+  //        resultStringBuilder.append(line).append("\n");
+  //      }
+  //    } catch (IOException e) {
+  //      throw new IOException("Error reading from input stream", e);
+  //    }
+  //    return resultStringBuilder.toString();
+  //  }
 }
