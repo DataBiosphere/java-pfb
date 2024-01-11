88862f3c79a422353c139733fea1c69f024bc8ea
Josh Ladieu <111856+jladieu@users.noreply.github.com>
2024-01-11 10:06:32 -0500

    Tweak `generate-test-files.sh` (#36)
    This change is in support of working with avro/pfb files for [AJ-1512](https://broadworkbench.atlassian.net/browse/AJ-1512).
    
    * Expect a fully qualified path to an .avro file, but don't assume it's in a particular directory.
    * If the file arg isn't provided, just regenerate from everything found in `$AVRO_DIR`.

--- tools/generate-test-files.sh
+++ tools/generate-test-files.sh
@@ -1,14 +1,60 @@
 #!/bin/bash
-# ASSUMES YOU'VE CHECKED OUT THE pypfb repo and are in the tools directory of java-pfb
+# Script: java-pfb/tools/generate-test-files.sh
+#
+# By default: This script scans library/src/test/resources/avro for Avro files and generates the output of
+# running various pfb commands on it.  The output is written into library/src/test/resources/pyPfbOutput
+#
+# An optional single argument can be provided to specify a single Avro file to generate test files for.
+set -euo pipefail
 
-set -eu
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+OUTPUT_DIR="$SCRIPT_DIR/../library/src/test/resources/pyPfbOutput"
 
-read -rp "Enter avro file name to generate test files for (Don't include .avro and it should live in the java-pfb/library/src/test/resources/avro directory): " AVRO_FILE_NAME
+# Ensure the output directories exist
+mkdir -p "$OUTPUT_DIR/show" "$OUTPUT_DIR/showMetadata" "$OUTPUT_DIR/showNodes" "$OUTPUT_DIR/showSchema"
 
-# generate test files
+generate_test_files() {
+    if [[ ! $1 =~ \.avro$ ]]; then
+        echo "Error: $1 does not have .avro extension"
+        exit 1
+    fi
 
-cd ../../pypfb
-pfb show -i ../java-pfb/library/src/test/resources/avro/${AVRO_FILE_NAME}.avro >> ../java-pfb/library/src/test/resources/pyPfbOutput/show/$AVRO_FILE_NAME.json
-pfb show -i ../java-pfb/library/src/test/resources/avro/${AVRO_FILE_NAME}.avro metadata >> ../java-pfb/library/src/test/resources/pyPfbOutput/showMetadata/$AVRO_FILE_NAME.json
-pfb show -i ../java-pfb/library/src/test/resources/avro/${AVRO_FILE_NAME}.avro nodes >> ../java-pfb/library/src/test/resources/pyPfbOutput/showNodes/$AVRO_FILE_NAME.txt
-pfb show -i ../java-pfb/library/src/test/resources/avro/${AVRO_FILE_NAME}.avro schema >> ../java-pfb/library/src/test/resources/pyPfbOutput/showSchema/$AVRO_FILE_NAME.json
+    # Ensure the specified AVRO file exists
+    if [ ! -f "$1" ]; then
+        echo "Error: file not found at $1"
+        exit 1
+    fi
+
+    local avro_filepath
+    avro_filepath=$(realpath "$1")
+
+    local output_dir
+    output_dir=$(realpath "$OUTPUT_DIR")
+
+    local output_filename
+    output_filename=$(basename "$avro_filepath" .avro)
+
+    pfb show -i "$avro_filepath" > "$output_dir/show/$output_filename.json"
+    pfb show -i "$avro_filepath" metadata > "$output_dir/showMetadata/$output_filename.json"
+    pfb show -i "$avro_filepath" nodes > "$output_dir/showNodes/$output_filename.txt"
+    pfb show -i "$avro_filepath" schema > "$output_dir/showSchema/$output_filename.json"
+
+    echo "Generated test files for $avro_filepath in $output_dir"
+}
+
+if [ "$#" -eq 1 ]; then
+    # If a filename is provided as an argument, generate only that file
+    generate_test_files "$1"
+else
+    AVRO_DIR="$SCRIPT_DIR/../library/src/test/resources/avro"
+
+    # Ensure the AVRO directory exists
+    if [ ! -d "$AVRO_DIR" ]; then
+        echo "Error: AVRO directory not found at $AVRO_DIR"
+        exit 1
+    fi
+    # Otherwise, generate all files in the AVRO directory
+    for avro_file_path in "$AVRO_DIR"/*; do
+        generate_test_files "$avro_file_path"
+    done
+fi
