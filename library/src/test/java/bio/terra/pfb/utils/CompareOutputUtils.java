ee2d5947b61f9e6f182f3b2746fb5289beafe0f4
David An <davidan@broadinstitute.org>
2026-04-13 10:55:30 -0400

    unused imports

--- library/src/test/java/bio/terra/pfb/utils/CompareOutputUtils.java
+++ library/src/test/java/bio/terra/pfb/utils/CompareOutputUtils.java
@@ -13,8 +13,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import org.skyscreamer.jsonassert.JSONAssert;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CompareOutputUtils {
 
