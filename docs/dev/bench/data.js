97e7f3bcc5251e17e48fc9b751196e0386a2979d
github-action-benchmark <github@users.noreply.github.com>
2026-09-11 20:25:27 +0000

    add Benchmark (jmh) benchmark result for ced7dad170a5ea43514857cc90fa7898688e9fc0

--- docs/dev/bench/data.js
+++ docs/dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789158110946,
+  "lastUpdate": 1789158327020,
   "repoUrl": "https://github.com/DataBiosphere/java-pfb",
   "entries": {
     "Benchmark": [
@@ -8564,6 +8564,46 @@ window.BENCHMARK_DATA = {
           {
             "name": "bio.terra.pfb.PfbReaderBenchmarks.convertEnum",
             "value": 6891652.936199922,
+            "unit": "ops/s",
+            "extra": "iterations: 2\nforks: 2\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robot@terra.team",
+            "name": "bumptagbot"
+          },
+          "committer": {
+            "email": "robot@terra.team",
+            "name": "bumptagbot"
+          },
+          "distinct": true,
+          "id": "ced7dad170a5ea43514857cc90fa7898688e9fc0",
+          "message": "bump 0.107.0",
+          "timestamp": "2026-09-11T20:21:23Z",
+          "tree_id": "ea0859abb034f412476b5471a5aab7c6a704fe67",
+          "url": "https://github.com/DataBiosphere/java-pfb/commit/ced7dad170a5ea43514857cc90fa7898688e9fc0"
+        },
+        "date": 1789158325240,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "bio.terra.pfb.LibraryBenchmarks.showNodesMedium",
+            "value": 1693.2328103631962,
+            "unit": "ops/s",
+            "extra": "iterations: 2\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "bio.terra.pfb.LibraryBenchmarks.showNodesSmall",
+            "value": 27826.60676069898,
+            "unit": "ops/s",
+            "extra": "iterations: 2\nforks: 2\nthreads: 1"
+          },
+          {
+            "name": "bio.terra.pfb.PfbReaderBenchmarks.convertEnum",
+            "value": 7872090.008437073,
             "unit": "ops/s",
             "extra": "iterations: 2\nforks: 2\nthreads: 1"
           }
